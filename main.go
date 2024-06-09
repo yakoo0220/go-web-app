f582439779c1efd674d3de50d0b4f77209cf74f7
建勋 张 <yakoo980220@outlook.com>
2024-06-09 21:00:44 +0800

    jibenwanchengle

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
+	"regexp"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,17 +17,8 @@ var (
 )
 
 func main() {
-	// 设置日志文件
-	logFile, err := os.OpenFile("/www/wwwroot/go-web-app/go.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
-	}
-	defer logFile.Close()
-
-	log.SetOutput(logFile)
-
-	// 数据库连接
-	db, err = sql.Open("mysql", "root:5f3831a5b18f4955@tcp(127.0.0.1:3306)/go_web_app")
+	var err error
+	db, err = sql.Open("mysql", "root:7ea5837b98d523dc@tcp(127.0.0.1:3306)/go_web_app")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,41 +33,98 @@ func main() {
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		activationCode := r.FormValue("activationCode")
-
-		if validateAndUseActivationCode(activationCode) {
+		if validateAndMarkLogin(activationCode) {
 			http.SetCookie(w, &http.Cookie{
 				Name:  "session_token",
-				Value: "some_session_token",
+				Value: activationCode, // Store activation code in the session token
 			})
 			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
 			return
 		} else {
-			http.Error(w, "Invalid or already used activation code", http.StatusUnauthorized)
+			templates.ExecuteTemplate(w, "login.html", map[string]interface{}{
+				"Error": "Invalid or already used activation code",
+				"Code":  activationCode,
+			})
 			return
 		}
 	}
 
 	code := r.URL.Query().Get("code")
-	templates.ExecuteTemplate(w, "login.html", code)
+	templates.ExecuteTemplate(w, "login.html", map[string]interface{}{
+		"Code": code,
+	})
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("session_token")
-	if err != nil || cookie.Value != "some_session_token" {
+	if err != nil || cookie.Value == "" {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
+	activationCode := cookie.Value
 
 	if r.Method == http.MethodPost {
-		userInput := r.FormValue("userInput")
-		templates.ExecuteTemplate(w, "dashboard.html", userInput)
-		return
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if !isValidEmail(username) || !isValidPassword(password) {
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+
+		if saveUserCredentialsAndMarkUsed(activationCode, username, password) {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		} else {
+			http.Error(w, "Failed to save user credentials", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	templates.ExecuteTemplate(w, "dashboard.html", nil)
 }
 
-func validateAndUseActivationCode(code string) bool {
+func isValidEmail(email string) bool {
+	re := regexp.MustCompile(`^[^\s@]+@[^\s@]+\.[^\s@]+$`)
+	return re.MatchString(email)
+}
+
+func isValidPassword(password string) bool {
+	if len(password) < 8 {
+		return false
+	}
+	hasUpper := regexp.MustCompile(`[A-Z]`).MatchString(password)
+	hasLower := regexp.MustCompile(`[a-z]`).MatchString(password)
+	hasNumber := regexp.MustCompile(`\d`).MatchString(password)
+	hasSpecial := regexp.MustCompile(`[@$!%*?&]`).MatchString(password)
+	return hasUpper && hasLower && hasNumber && hasSpecial
+}
+
+func saveUserCredentialsAndMarkUsed(activationCode, username, password string) bool {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println("Error starting transaction:", err)
+		return false
+	}
+
+	now := time.Now()
+	_, err = tx.Exec("UPDATE activation_codes SET email = ?, password = ?, used = TRUE, registration_time = ?, login_time = ? WHERE code = ?", username, password, now, now, activationCode)
+	if err != nil {
+		log.Println("Error updating user credentials:", err)
+		tx.Rollback()
+		return false
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Println("Error committing transaction:", err)
+		return false
+	}
+
+	return true
+}
+
+func validateAndMarkLogin(code string) bool {
 	tx, err := db.Begin()
 	if err != nil {
 		log.Println("Error starting transaction:", err)
@@ -83,7 +132,7 @@ func validateAndUseActivationCode(code string) bool {
 	}
 
 	var count int
-	err = tx.QueryRow("SELECT COUNT(*) FROM activation_codes WHERE code = ? AND used = FALSE", code).Scan(&count)
+	err = tx.QueryRow("SELECT COUNT(*) FROM activation_codes WHERE code = ? AND used = FALSE AND islogin = FALSE", code).Scan(&count)
 	if err != nil {
 		log.Println("Error querying database:", err)
 		tx.Rollback()
@@ -95,7 +144,8 @@ func validateAndUseActivationCode(code string) bool {
 		return false
 	}
 
-	_, err = tx.Exec("UPDATE activation_codes SET used = TRUE WHERE code = ?", code)
+	now := time.Now()
+	_, err = tx.Exec("UPDATE activation_codes SET islogin = TRUE, login_time = ? WHERE code = ?", now, code)
 	if err != nil {
 		log.Println("Error updating database:", err)
 		tx.Rollback()
